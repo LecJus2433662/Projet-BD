896bc1ce4ad0cf78c617eb96743b1553b6b88417
Justin Leclerc <2433662@etu.cegepjonquiere.ca>
2025-11-14 14:00:57 -0500

    fix BD

--- Projet_interface_BD/BD_ProjetBlazor/BD_ProjetBlazor/Services/Requete_inscriptions.cs
+++ Projet_interface_BD/BD_ProjetBlazor/BD_ProjetBlazor/Services/Requete_inscriptions.cs
@@ -1,29 +1,37 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System.Globalization;
-using BD_ProjetBlazor.Data;
+﻿using BD_ProjetBlazor.Data;
 using BD_ProjetBlazor.Models;
-namespace BD_ProjetBlazor.Services
-{
-    public class Requete_inscriptions
-    {
-        private readonly IDbContextFactory<ProgA25BdProjetProgContext> _dbContextFactory;
-        public Requete_inscriptions(IDbContextFactory<ProgA25BdProjetProgContext> dbContextFactory)
-        {
-            _dbContextFactory = dbContextFactory;
-        }
-        public async Task<List<Utilisateur>> GetInscriptions()
-        {
-             var _context = _dbContextFactory.CreateDbContextAsync().Result;
-            var user =  from u in _context.Utilisateurs
-                       select u;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System.Text;
 
-            return user.ToListAsync().Result;
-        }
-        public async Task AjouterUtilisateurAsync(Utilisateur utilisateur)
-        {
-            using var context = await _dbContextFactory.CreateDbContextAsync();
-            context.Utilisateurs.Add(utilisateur);
-            await context.SaveChangesAsync();
-        }
+public class Requete_inscriptions
+{
+    private readonly IDbContextFactory<ProgA25BdProjetProgContext> _dbContextFactory;
+
+    public Requete_inscriptions(IDbContextFactory<ProgA25BdProjetProgContext> dbContextFactory)
+    {
+        _dbContextFactory = dbContextFactory;
+    }
+
+    public async Task<List<Utilisateur>> GetInscriptions()
+    {
+        await using var _context = await _dbContextFactory.CreateDbContextAsync();
+        return await _context.Utilisateurs.ToListAsync();
+    }
+
+    public async Task AjouterUtilisateur(Utilisateur utilisateur, string motDePasse)
+    {
+        await using var _context = await _dbContextFactory.CreateDbContextAsync();
+
+        // Générer un sel aléatoire
+        utilisateur.Sel = Guid.NewGuid();
+
+        // Convertir le mot de passe en byte[] (BINARY(64)) avec SHA-256 + sel
+        using var sha256 = SHA256.Create();
+        byte[] passwordBytes = Encoding.UTF8.GetBytes(motDePasse + utilisateur.Sel.ToString());
+        utilisateur.MotDePasse = sha256.ComputeHash(passwordBytes);
+
+        _context.Utilisateurs.Add(utilisateur);
+        await _context.SaveChangesAsync();
     }
 }
