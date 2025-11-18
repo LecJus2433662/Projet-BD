b22ea5942e488587b333da46312c854d69fd54fe
Justin Leclerc <2433662@etu.cegepjonquiere.ca>
2025-11-18 08:51:33 -0500

    inscription presque fini

--- Projet_interface_BD/BD_ProjetBlazor/BD_ProjetBlazor/Services/Requete_inscriptions.cs
+++ Projet_interface_BD/BD_ProjetBlazor/BD_ProjetBlazor/Services/Requete_inscriptions.cs
@@ -1,33 +1,41 @@
-﻿using BD_ProjetBlazor.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using BD_ProjetBlazor.Data;
 using BD_ProjetBlazor.Models;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
-using System.Security.Cryptography;
-using System.Text;
 
-public class Requete_inscriptions
+namespace BD_ProjetBlazor.Services
 {
-    private readonly IDbContextFactory<ProgA25BdProjetProgContext> _dbContextFactory;
-
-    public Requete_inscriptions(IDbContextFactory<ProgA25BdProjetProgContext> dbContextFactory)
+    public class Requete_inscriptions
     {
-        _dbContextFactory = dbContextFactory;
-    }
+        private readonly IDbContextFactory<ProgA25BdProjetProgContext> _dbContextFactory;
 
-    public async Task<List<Utilisateur>> GetInscriptions(string email)
-    {
-       var dbContext = _dbContextFactory.CreateDbContextAsync().Result;
-        var user = from u in dbContext.Utilisateurs
-                   where u.Email == email
-                  select u;
-        return await dbContext.Utilisateurs.ToListAsync();
-    }
-    public async Task<List<Utilisateur>> InsertUser()
-    {
-        var dbContext = _dbContextFactory.CreateDbContextAsync().Result;
-        
-                   
-    }
+        public Requete_inscriptions(
+            IDbContextFactory<ProgA25BdProjetProgContext> dbContextFactory)
+        {
+            _dbContextFactory = dbContextFactory;
+        }
+        public async Task AjouterUtilisateur(
+            string email,
+            byte[] motDePasse,
+            string prenom,
+            string nom,
+            string ville,
+            string pays)
+        {
+            var dbContext = await _dbContextFactory.CreateDbContextAsync();
 
-    
+            var utilisateur = new Utilisateur
+            {
+                Email = email,
+                MotDePasse = motDePasse,
+                Sel = Guid.NewGuid(),
+                Prenom = prenom,
+                Nom = nom,
+                Ville = ville,
+                Pays = pays
+            };
+
+            dbContext.Utilisateurs.Add(utilisateur);
+            await dbContext.SaveChangesAsync();
+        }
+    }
 }
